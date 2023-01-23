676e1cd05e8d5571e46713dc645c2bcac27bdbe0
David <gmz_cat@hotmail.com>
2023-01-23 10:25:48 +0100

    Nivell 1

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
       />
     ))}
     <div>
-    <ButtonStyle onClick={() => Hello(console.log('Hello'))}> HELLO </ButtonStyle>
+    <ButtonStyle> HELLO </ButtonStyle>
     </div>
   </>
 }
